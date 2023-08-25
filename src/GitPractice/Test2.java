2a1f5a4234b794e6c0da579c6a5923128378329b
bvp13 <bvp13@BhushanPatil>
2023-08-25 14:54:59 +0530

    updated code

---
+++ src/GitPractice/Test2.java
@@ -0,0 +1,14 @@
+package GitPractice;
+
+public class Test2
+{
+	public class Test2
+	{
+	public static void main(String[] args) 
+	{
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.facebook.com/");
+	}
+	}
+
+}
