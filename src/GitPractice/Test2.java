09a8f516a204cdee8104bd59818084cfa2c52819
bvp13 <bvp13@BhushanPatil>
2023-08-25 14:28:34 +0530

    new program is updated

--- src/GitPractice/Test2.java
+++ src/GitPractice/Test2.java
@@ -3,13 +3,14 @@ package GitPractice;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Test1 
+public class Test2 
 {
 	public static void main(String[] args) throws InterruptedException 
 	{
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.amazon.com/");
+		driver.get("https://www.facebook.com/");
 		
+
 		Thread.sleep(3000);
 		
 		driver.close();
